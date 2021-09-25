ae5140e2cea75db25c0613e10cc6c141a817bea7
fpatel7 <79761974+fpatel7@users.noreply.github.com>
2021-09-24 22:33:52 -0500

    completed

--- src/edu/jsu/mcis/cs310/Main.java
+++ src/edu/jsu/mcis/cs310/Main.java
@@ -1,12 +1,23 @@
 package edu.jsu.mcis.cs310;
 
+import edu.jsu.mcis.cs310.Main;
+import static edu.jsu.mcis.cs310.DatabaseTest.getJSONData;
+import org.json.simple.JSONArray;
+
+/**
+ *
+ * @author dustn
+ */
 public class Main {
-
-    public static void main(String[] args) {
+     
+        public static void main(String[] args) {
+                // Convert DataBase to JSON; 
         
-        DatabaseTest db = new DatabaseTest();
-        db.dbTest();
+        System.out.println("CONVERSION RESULTS (DataBase to JSON)");
+        System.out.println("================================");
 
+        //String json = Converter.csvToJson(csvFileString);
+        JSONArray json = getJSONData();
+        System.out.println(json);
     }
-
 }
